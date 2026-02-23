cc5c70b6080bd4829174966fd0447fee7b5763fb
tuutti <tuutti@iki.fi>
2026-02-23 09:30:09 +0200

    UHF-9462: Drush command to scan composer patches

---
+++ src/Drush/Commands/ComposerPatchesCommands.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Consolidation\AnnotatedCommand\CommandResult;
+use Drush\Attributes\Argument;
+use Drush\Attributes\Bootstrap;
+use Drush\Attributes\Command;
+use Drush\Attributes\FieldLabels;
+use Drush\Boot\DrupalBootLevels;
+use Drush\Commands\DrushCommands;
+
+/**
+ * A drush command to check composer patches.
+ */
+final class ComposerPatchesCommands extends DrushCommands {
+
+  /**
+   * Checks whether Composer patches are installed correctly.
+   *
+   * @param string $file
+   *   The path to composer.lock file.
+   * @param array $options
+   *   The options.
+   *
+   * @return \Consolidation\AnnotatedCommand\CommandResult
+   *   The result.
+   */
+  #[Command(name: 'helfi:tools:check-composer-patches')]
+  #[Bootstrap(level: DrupalBootLevels::NONE)]
+  #[Argument(name: 'file', description: 'Path to composer.lock file')]
+  #[FieldLabels(labels: [
+    'name' => 'Name',
+    'version' => 'Current version',
+    'latest' => 'Latest version',
+  ])]
+  public function checkVersions(string $file, array $options = ['format' => 'table']) : CommandResult {
+  }
+
+}
+
---
+++ src/Drush/Listeners/ServiceListener.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrupalTools\Drush\Listeners;
+
+use Drush\Event\ConsoleDefinitionsEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+#[AsEventListener]
+final class ServiceListener {
+
+  public function __invoke(ConsoleDefinitionsEvent $event): void {
+    $x = 1;
+  }
+
+}
